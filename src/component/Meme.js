eca35ac7eb3299e49b77658261672fab39148bb8
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-25 04:33:43 +0100

    added Meme component with two input & button

---
+++ src/component/Meme.js
@@ -0,0 +1,11 @@
+export default function Meme() {
+    return (
+        <div>
+           <form className="form-control">
+                <input />
+                <input />
+                <button></button>
+           </form>
+        </div>
+    )
+}
