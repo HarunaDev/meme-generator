2a5972eb7c2a2d3785b6585e6c14ed869d6aabac
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-25 04:50:58 +0100

    added styling for form  elements

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -1,10 +1,10 @@
 export default function Meme() {
     return (
-        <div>
+        <div className="main">
            <form className="form-control">
-                <input />
-                <input />
-                <button></button>
+                <input placeholder="top text" className="form-input" type="text"/>
+                <input placeholder="bottom text" className="form-input" type="text"/>
+                <button className="form-button">Generate Random Meme</button>
            </form>
         </div>
     )
